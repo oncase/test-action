552d4ff8967a6457e80a637fa44c380d9f65663f
Enriqson <elaborao@hotmail.com>
2022-06-29 13:20:37 -0300

    test action implementation

--- index.js
+++ index.js
@@ -1,21 +1,92 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require("@actions/core");
+const axios = require("axios");
+const fs = require("fs");
+const newman = require("newman");
+const { exit } = require("process");
 
+function getDataFromPostman(url) {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  axios.defaults.headers.common = {
+    "X-API-Key": core.getInput("postman_api_key"),
+  };
+  return axios({
+    method: "get",
+    url: url,
+    config,
+  }).then((res) => res.data);
+}
 
-// most @actions toolkit packages have async methods
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+  //data from postman
+  const collectionUrl =
+    core.getInput("postman_api_url") +
+    "collections/" +
+    core.getInput("postman_collection_id");
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+  const envUrl =
+    core.getInput("postman_api_url") +
+    "environments/" +
+    core.getInput("postman_env_id");
 
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
+  const collection = await getDataFromPostman(collectionUrl);
+  const postmanEnv = await getDataFromPostman(envUrl);
+
+  //missing tests check
+  const testUrls = collection.collection.item.map((item) =>
+    item.request.url.raw.replace("{{host}}", "")
+  );
+
+  const apiUrls = JSON.parse(
+    fs.readFileSync(core.getInput("url_list_filename"), "utf8")
+  );
+
+  const apiListKey = Object.keys(apiUrls)[0];
+
+  const missingTestUrls = [];
+
+  for (let url of apiUrls[apiListKey]) {
+    if (!testUrls.includes(url)) missingTestUrls.push(url);
   }
+
+  if (missingTestUrls.length) {
+    const missingTestMsg = `Urls ${missingTestUrls} have no registered tests`;
+    console.error(missingTestMsg);
+    if (core.getInput("continue_if_test_missing") !== "true") exit(-1);
+  }
+
+  //running tests
+  newman.run(
+    {
+      collection: collection.collection,
+      environment: postmanEnv.environment,
+      reporters: "@oncase/slackmsg",
+      reporter: {
+        "@oncase/slackmsg": {
+          webhookurl:
+            "https://hooks.slack.com/services/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",
+        },
+      },
+    },
+    function (err, summary) {
+      if (err) {
+        console.error(err);
+        exit(-1);
+      }
+
+      if (summary.run.failures.length) {
+        const testNames = summary.run.failures.map((item) => item.source.name);
+        const failedTestsMsg = `Tests ${testNames} have failed`;
+        console.error(failedTestsMsg);
+
+        if (core.getInput("continue_if_fail") !== "true") exit(-1);
+      }
+      console.log("collection run complete!");
+    }
+  );
 }
 
 run();
--- index.test.js
+++ index.test.js
@@ -1,24 +1,29 @@
-const wait = require('./wait');
-const process = require('process');
-const cp = require('child_process');
-const path = require('path');
-
-test('throws invalid number', async () => {
-  await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
-});
-
-test('wait 500 ms', async () => {
-  const start = new Date();
-  await wait(500);
-  const end = new Date();
-  var delta = Math.abs(end - start);
-  expect(delta).toBeGreaterThanOrEqual(500);
-});
+const process = require("process");
+const cp = require("child_process");
+const path = require("path");
+const fs = require("fs");
+require("dotenv").config();
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 100;
-  const ip = path.join(__dirname, 'index.js');
-  const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
+test("test runs", () => {
+  fs.writeFileSync(
+    process.env.INPUT_URL_LIST_FILENAME,
+    JSON.stringify({
+      data: [
+        "/rec-customer-product",
+        "/event",
+        "/year",
+        "/user",
+        "/city",
+        "/port",
+      ],
+    }),
+    "utf8"
+  );
+
+  const ip = path.join(__dirname, "index.js");
+  const result = cp.execSync(`node ${ip}`, { env: process.env }).toString();
   console.log(result);
-})
+
+  fs.unlinkSync(process.env.INPUT_URL_LIST_FILENAME);
+});
